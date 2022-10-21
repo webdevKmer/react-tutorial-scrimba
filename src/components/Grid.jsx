edc5d8fd67ed962126d6d7ed2f6bee52c551deec
webdevKmer <webdevKmer@gmail.com>
2022-10-21 06:31:27 +0000

    rendering data fromarray

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Card from "./Card"
+import data from '../data'
 
 function Grid() {
+  let eltArr = data
+
+  const cardsJSX = eltArr.map(elt => (
+    <Card key={elt.id} img={elt.coverImg} rating={elt.stats.rating} reviewCount={elt.stats.reviewCount} country={elt.location} title={elt.title} price={elt.price} />
+  ))
   return (
     <div className="grid">
-      <Card 
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount="6"
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price="136"/>
+      {cardsJSX}
     </div>
   )
 }
