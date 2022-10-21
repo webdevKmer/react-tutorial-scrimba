bf6bdbe98fe70e9d40222c471c83efafde8aa3fe
Dev Kmer <webdevKmer@gmail.com>
2022-10-21 12:33:44 +0100

    finishing layout and fixint style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Card(props) {
+  console.log(props);
   return (
     <div className="card">
-            
+            <div className="place-image">
+              <img src={props.imageUrl} alt="place" />
+            </div>
+            <div className="place-details">
+              <div className="location">
+                <img src="../images/star.png" alt="icon" />
+                <div className="upper">{props.location}</div>
+                <a href={props.googleMapsUrl} target="_blank" rel="noopener noreferrer">Link to Google Maps</a>
+              </div>
+              <h2 className="title">{props.title}</h2>
+              <p className="period">{props.startDate} - {props.endDate}</p>
+              <p className="description">{props.description}</p>
+            </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <img className="logo" src='../images/star.png' alt='logo'/>
+      <div>My Travel Journal.</div>
     </nav>
   )
 }
