a69e7b57a3c1802d68743ecedee179f064389e28
Dev Kmer <109858799+webdevKmer@users.noreply.github.com>
2022-10-23 08:30:49 +0100

    fetching data from star wars API

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BoxGrid from './BoxGrid'
 import NewsletterForm from './NewsletterForm'
+import StarWarsApi from './StarWarsApi'
 
 function MainComponent() {
   return (
     <div className="main">
+      <StarWarsApi />
       <NewsletterForm />
       <BoxGrid darkMode={true} />
     </div>
