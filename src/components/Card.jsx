ab51f51b20e4af611f1603da0e3d43b5cbaf79c0
webdevKmer <webdevKmer@gmail.com>
2022-10-29 15:54:55 +0000

    finishing the app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
 function Card(props) {
+  let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
   return (
     <div className="card">
+            {badgeText && <div className="badge-text">{badgeText}</div>}            
             <img src={`../images/${props.img}`} className="card--image" alt='sportler'/>
             <div className="card--stats">
-                <img src='../images/star.png' className="card--star" alt='start logo'/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <div className='rating'>
+                  <img src='../images/star.png' className="card--star" alt='start logo'/>
+                  <span>{props.rating}</span>
+                </div>
+                <span className="gray">({props.reviewCount}) •</span>
                 <span className="gray">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <div>
+              <p>{props.title}</p>
+              <p><span className="bold">From ${props.price}</span> / Person</p>
+            </div>
         </div>
   )
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,7 +6,16 @@ function Grid() {
   let eltArr = data
 
   const cardsJSX = eltArr.map(elt => (
-    <Card key={elt.id} img={elt.coverImg} rating={elt.stats.rating} reviewCount={elt.stats.reviewCount} country={elt.location} title={elt.title} price={elt.price} />
+    <Card 
+    key={elt.id} 
+    img={elt.coverImg} 
+    rating={elt.stats.rating} 
+    reviewCount={elt.stats.reviewCount} 
+    location={elt.location} 
+    title={elt.title} 
+    price={elt.price}
+    openSpots = {elt.openSpots}
+    />
   ))
   return (
     <div className="grid">
