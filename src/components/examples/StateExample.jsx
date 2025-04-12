92f5c5bd89a3cbb1e5242ff8c0c31592fd87437a
Dev Kmer <webdev.kmer@gmail.com>
2025-04-12 04:01:54 +0100

    completed state example component

--- src/components/examples/StateExample.jsx
+++ src/components/examples/StateExample.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './StateExample.css'
 
 const StateExample = () => {
-    const [answer, setAnswer] = useState('Yes')
-    const handleClick = () => setAnswer(answer=='Yes'?'Clicked':'Yes')
+    const [answer, setAnswer] = useState(true)
+    const handleClick = () => setAnswer(prev => !prev)
   return (
     <section className='state'>
         <h2>Is State important to know</h2>
-        <button onClick={handleClick}>{answer}</button>
+        <button onClick={handleClick}>{answer?'Yes':'No'}</button>
     </section>
   )
 }
