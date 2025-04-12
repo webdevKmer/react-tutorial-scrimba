70d8b4b5fedf1e69087bb7d7456ba8ec1bf5f9c5
Dev Kmer <webdev.kmer@gmail.com>
2025-04-12 04:04:47 +0100

    fixed the main app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import Header from './components/chef-claude/Header';
 import Main from './components/chef-claude/Main';
 import Footer from './components/chef-claude/Footer';
-import Form from './components/chef-claude/Form';
-import StateExample from './components/examples/StateExample';
+import ComplexStateObjects from './components/examples/ComplexStateObjects';
 
 function App() {
   return (
     <>
       <div className="App">
-        <Header />
-        <Form />
+        {/* <Header />
         <Main />
-        <StateExample />
-        <Footer />
+        <Footer /> */}
+        <ComplexStateObjects />
       </div>
     </>
   );
--- src/components/chef-claude/Main.jsx
+++ src/components/chef-claude/Main.jsx
@@ -1,9 +1,28 @@
 import "./Main.css"
+import "./Form.css"
+import { useState } from "react"
+
+
 const Main = () => {
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+  const [ingredient, setIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+
   const ingredientsList = ingredients.map(ingredient => <li>{ingredient}</li>)
+  const handleChange = (e) => {
+    setIngredient(e.target.value)
+  }
+  const addIngredient = (e) => {
+    e.preventDefault()
+    setIngredients(prev => [...prev, ingredient])
+    setIngredient("")
+  }
+
   return (
     <main>
+      <form onSubmit={addIngredient}>
+        <input onChange={handleChange} type="text" name="ingredient" id="" placeholder="e.g banana" value={ingredient}/>
+        <button>Add ingredient</button>
+    </form>
       <section>
         <h2>Ingredients on Hand :</h2>
         <ul>
